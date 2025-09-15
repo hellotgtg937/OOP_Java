19842429b11bcf198bdb2f9321bad9dcbecb87e2
Kim tg <hellotgtg937@naver.com>
2025-09-15 10:41:31 +0900

    OOP week 1

---
+++ src/ch02/sec03/CharExample.java
@@ -0,0 +1,25 @@
+package ch02.sec03;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c1 = 'A'; // char은 문자 리터럴이라서 문자 하나만 저장 가
+		char c2 = 65; 		
+		char c3 = '가';
+		char c4 = 44032; // 유니코드 직접 저장
+		
+		char c5 = 'f';
+		
+		String c6 = "hello"; //문자열을 저장할 거면 string을 써야한다.
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);		
+
+	}
+
+}
