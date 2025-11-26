57b438071d95285fa5f551c726157039a3755ccd
Kim tg <hellotgtg937@naver.com>
2025-11-26 21:42:29 +0900

    12주차 + 과제

---
+++ src/task01/Account.java
@@ -0,0 +1,40 @@
+package task01;
+
+public class Account {
+	
+	private String no;
+	private String name;
+	private int balance;
+	
+	public Account (String no, String name, int balance) {
+		this.no = no;
+		this.name = name;
+		this.balance = balance;
+	}
+	
+	public String getNo() {
+		return no;
+	}
+	
+	public void setNo(String no) {
+		this.no = no;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+
+}
